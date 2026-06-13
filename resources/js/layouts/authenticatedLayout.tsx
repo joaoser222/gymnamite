e4a362fec160f3bdfddc791a0cbae8db591f97d2
joaoser222 <joaoser222@gmail.com>
2026-06-13 15:56:49 -0300

    fix: corr. utilização de layout

--- resources/js/layouts/authenticatedLayout.tsx
+++ resources/js/layouts/authenticatedLayout.tsx
@@ -1,6 +1,3 @@
-import type { ReactNode } from 'react';
 import AppLayout from '@/layouts/AppLayout';
 
-export function authenticatedLayout(page: ReactNode): ReactNode {
-    return <AppLayout>{page}</AppLayout>;
-}
+export const authenticatedLayout = AppLayout;
