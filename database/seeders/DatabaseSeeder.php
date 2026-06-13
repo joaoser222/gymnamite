bffe64b4bd140040a3ce139a8f10b30bbf56b24e
joaoser222 <joaoser222@gmail.com>
2026-06-13 15:33:54 -0300

    chore: adc. usuário administrador de teste no seed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,11 +15,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
 
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name' => 'Administrador',
+            'email' => 'admin@mail.com',
+            'password' => '987654321'
         ]);
     }
 }
