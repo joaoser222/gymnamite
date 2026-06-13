8e1bfed8abca5f09a41e443359626b7ce85d794a
joaoser222 <joaoser222@gmail.com>
2026-06-13 15:30:13 -0300

    fix: rem. exibição de roles

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -15,7 +15,6 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('description');
-            $table->string('visibility', 10);
             $table->timestamps();
         });
 
