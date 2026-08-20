79547b5896e5514d36ea07256b5271d0cc0f761f
joaoser222 <joaoser222@gmail.com>
2026-08-20 19:44:40 +0000

    test(tests): desativa CSRF nos testes de feature

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,10 +2,20 @@
 
 namespace Tests;
 
+use Illuminate\Foundation\Http\Middleware\PreventRequestForgery;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 use Tests\Concerns\InteractsWithPermissions;
 
 abstract class TestCase extends BaseTestCase
 {
     use InteractsWithPermissions;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        // Web form feature tests post without a CSRF token; disable the
+        // request forgery middleware for the test environment only.
+        $this->withoutMiddleware(PreventRequestForgery::class);
+    }
 }
